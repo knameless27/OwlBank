8f3306582cfe6634fe4aee03c55201eaeaf097a5
knameless27 <knameless27@gmail.com>
2025-03-05 17:01:53 -0500

    first commit :tada:

---
+++ src/main/java/com/knameless/OwlBank/OwlBankApplication.java
@@ -0,0 +1,14 @@
+package com.knameless.OwlBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlBankApplication {
+
+	public static void main(String[] args) {
+		System.out.println("waos");
+		SpringApplication.run(OwlBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knameless/OwlBank/OwlBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knameless.OwlBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
